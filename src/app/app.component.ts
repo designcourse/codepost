17122c23798399a33df671d058689a013f9dfe6a
Gary Simon <contact@designcourse.com>
2017-05-02 20:18:28 -0400

    learnhow stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { routerTransition } from './animations';
-
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   host: {'[@routerTransition]': ''},
-  animations: [routerTransition],
+  animations: [routerTransition,
+  
+        trigger('animateIn', [
+
+      state('in', style({transform: 'translateY(0)'})),
+
+      transition('void => *', [
+        style({transform: 'translateY(-100%)',opacity: '0'}),
+        animate('300ms 2s ease-out')
+      ])
+  ]),  
+  ],
 })
 export class AppComponent {
   title = 'app works!';
+  state = 'in';
 }
